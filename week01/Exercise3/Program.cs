ca890c41fc7e077dd91e33afb8ff453443ad2d1f
TheRAlex <acumanaosorio@byupathway.edu>
2026-05-07 21:18:27 -0400

    Excercise 3 assignment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+       
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1,11);
+
+        int guess = -1;
+
+        while (magicnumber != guess)
+        {
+        Console.WriteLine($"What is the magic number {magicnumber}");
+        Console.WriteLine($"What is your guess?");
+        guess = int.Parse(Console.ReadLine());
+
+        if (guess > magicnumber)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else if (guess == magicnumber)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+        else
+        {
+            Console.WriteLine("Higher");
+        }
+
+            
+        }
+        
+
+
+
+
     }
 }
