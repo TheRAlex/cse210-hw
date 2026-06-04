6080f39b2bb032238d11d31832f98120db99764e
TheRAlex <acumanaosorio@byupathway.edu>
2026-06-03 20:37:07 -0400

    Homework Assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Jorge Zapata", "Art");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Alexander Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Nicole James", "Informatics", "CyberSecurity fundamentals");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
